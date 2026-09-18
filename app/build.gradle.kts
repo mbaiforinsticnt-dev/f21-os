baf7376d072d42e178062569f00a2b620b192753
mbaiforinsticnt-dev <mbaiforinsticnt@gmail.com>
2026-09-18 19:47:49 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,12 +3,18 @@ plugins { id("com.android.application") }
 android {
     namespace = "dev.mbaiforinstinct.f21os"
     compileSdk = 35
-
     defaultConfig {
         applicationId = "dev.mbaiforinstinct.f21os"
         minSdk = 30
         targetSdk = 30
         versionCode = 1
         versionName = "0.1.0"
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
+}
+
+dependencies {
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test:runner:1.6.2")
+    androidTestImplementation("androidx.test:rules:1.6.1")
 }
