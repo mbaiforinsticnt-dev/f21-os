df1567eae6ca6a3502566bbfebeb8f347176e543
mbaiforinsticnt-dev <mbaiforinsticnt@gmail.com>
2026-09-18 15:02:01 +0100

    Create build.gradle.kts

---
+++ app/build.gradle.kts
@@ -0,0 +1,14 @@
+plugins { id("com.android.application") }
+
+android {
+    namespace = "dev.mbaiforinstinct.f21os"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "dev.mbaiforinstinct.f21os"
+        minSdk = 30
+        targetSdk = 30
+        versionCode = 1
+        versionName = "0.1.0"
+    }
+}
