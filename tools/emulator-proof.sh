609e29b7ff7142be7e2352e833f1c6a8ba0172ad
mbaiforinsticnt-dev <mbaiforinsticnt@gmail.com>
2026-09-18 19:59:53 +0100

    Update emulator-proof.sh

--- tools/emulator-proof.sh
+++ tools/emulator-proof.sh
@@ -6,5 +6,5 @@ $ADB install -r app/build/outputs/apk/debug/app-debug.apk
 $ADB install -r app/build/outputs/apk/androidTest/debug/app-debug-androidTest.apk
 $ADB shell am instrument -w dev.mbaiforinstinct.f21os.test/androidx.test.runner.AndroidJUnitRunner
 for f in idle.png menu.png menu-key-navigation.png; do
-  $ADB pull "/sdcard/Android/data/dev.mbaiforinstinct.f21os/files/proof/$f" "screenshots/$f"
+  $ADB exec-out run-as dev.mbaiforinstinct.f21os cat "files/proof/$f" > "screenshots/$f"
 done
